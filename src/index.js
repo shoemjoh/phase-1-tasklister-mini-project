f156bd5240bc1668f7b594567087b27d04b278f3
John <shoemjoh@gmail.com>
2023-11-11 15:51:58 -0500

    task builder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTask(e.target.newtaskdescription.value)
+    document.querySelector('form').reset();
+  })
 });
+
+function addTask(task) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
